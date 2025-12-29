ef72a9b787c95daca418fbc8e27d0541624b91e9
JafarTheAlien <cefertarverdiyevv@gmail.com>
2025-12-29 10:27:42 +0400

    JavaScript oyrenirik biz!

---
+++ jslearning/main.js
@@ -0,0 +1,8 @@
+// Valeh muelllim salam
+
+let answer  = prompt("Salam, Doguldugum ayi texmin et! ");
+
+answer ==  "noyabr"|| answer=="Noyabr" || answer== "noyabir" ? 
+(answer=="noyabir" ? 
+console.log("Duz yazmisane amma noyabr i siz yazilir!"): console.log("duzdu tebrikler")): console.log("Ayib olsun, bulmirsen?"  );
+   
