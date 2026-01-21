ddb8eef9b94490d8d9ba6f7d00e4f2dc045b0fdc
JafarTheAlien <cefertarverdiyevv@gmail.com>
2026-01-21 09:01:12 +0400

    Timer games

---
+++ exercises/main.js
@@ -0,0 +1,30 @@
+let h1 = document.querySelector("h1")
+// let downloadBox = document.querySelector(".download-bar")
+let btn = document.querySelector("button")
+
+
+let timer = 0
+
+let random = Math.floor(Math.random() * 3) + 2
+
+
+setTimeout(() => {
+    let k = setInterval(() => {
+        timer++
+    }, 1);
+    btn.innerText="click now!"
+    btn.addEventListener("click", () => {
+        timer = 10000-timer*10
+        clearInterval(k)
+        h1.innerText = "Your score: "+timer
+
+
+    }
+
+    );
+
+
+}, random * 1000)
+
+
+
